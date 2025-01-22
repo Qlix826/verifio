69949a0e22949960ea1803dafd15d9d52f899a68
echetoui <elyes.chetoui@cybernow.io>
2025-01-21 20:10:08 -0500

    fix: remove duplicate metadata import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { metadata } from './metadata'
 import type { Metadata } from 'next'
 import { TranslationsProvider } from '@/contexts/translations-context'
 
