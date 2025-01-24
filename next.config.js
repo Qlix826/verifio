86c241c96e968d32ff9de0bb75f6428ba5f8288c
echetoui <elyes.chetoui@cybernow.io>
2025-01-23 20:01:19 -0500

    Prepare for AWS deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+    unoptimized: true,
   },
   env: {
     AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
@@ -10,6 +11,7 @@ const nextConfig = {
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   },
   poweredByHeader: false,
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
